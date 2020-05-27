4ea595f4613bd7655af6d22b794022bbe6b45e9b
Bindra <ashishbindra2@gmail.com>
2020-05-27 16:54:32 +0530

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
 // DB Params
 define("DB_HOST", "localhost");
-define("DB_USER", "journals");
-define("DB_PASS", "journals*001");
-define("DB_NAME", "journals");
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_NAME", "");
 
 
 
